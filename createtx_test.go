97a942e49910aed292bfe9b796da2e476db713ef
Josh Rickmar <jrick@conformal.com>
2013-10-13 19:25:47 -0400

    fix tests

--- createtx_test.go
+++ createtx_test.go
@@ -41,7 +41,7 @@ func TestFakeTxs(t *testing.T) {
 		t.Errorf("Cannot decode address: %s", err)
 		return
 	}
-	copy(utxo.Addr[:], addr160)
+	copy(utxo.AddrHash[:], addr160)
 	ophash := (btcwire.ShaHash)([...]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
 		12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
 		28, 29, 30, 31, 32})
