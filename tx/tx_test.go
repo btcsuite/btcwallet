a12eb62b4d10a84c7d1e6a2c58053e1c0ea50c99
Josh Rickmar <jrick@conformal.com>
2013-10-22 10:03:11 -0400

    Make tests build again.

--- tx/tx_test.go
+++ tx/tx_test.go
@@ -85,7 +85,7 @@ var (
 
 func TestUtxoWriteRead(t *testing.T) {
 	utxo1 := &Utxo{
-		Addr: [ripemd160.Size]byte{
+		AddrHash: [ripemd160.Size]byte{
 			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
 			16, 17, 18, 19,
 		},
