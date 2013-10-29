9094bcde02c2872fd459a18264600ef6d67816a0
Josh Rickmar <jrick@conformal.com>
2013-10-29 11:02:30 -0400

    Remove unused const.

--- log.go
+++ log.go
@@ -22,15 +22,6 @@ import (
 	"os"
 )
 
-const (
-	// lockTimeThreshold is the number below which a lock time is
-	// interpreted to be a block number.  Since an average of one block
-	// is generated per 10 minutes, this allows blocks for about 9,512
-	// years.  However, if the field is interpreted as a timestamp, given
-	// the lock time is a uint32, the max is sometime around 2106.
-	lockTimeThreshold uint32 = 5e8 // Tue Nov 5 00:53:20 1985 UTC
-)
-
 var (
 	log = seelog.Disabled
 )
