4ee9ce59fba801ff5036de35afac67b61e6247cc
Josh Rickmar <jrick@companyzero.com>
2015-11-19 14:43:54 -0500

    Add sweepaccount tool.
    This tool creates on-chain transactions, one per used address in a
    source account, to sweep all output value to new addresses in a
    different destination account.

---
+++ internal/cfgutil/amount.go
@@ -0,0 +1,38 @@
+// Copyright (c) 2015-2016 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package cfgutil
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/btcsuite/btcutil"
+)
+
+// AmountFlag embeds a btcutil.Amount and implements the flags.Marshaler and
+// Unmarshaler interfaces so it can be used as a config struct field.
+type AmountFlag struct {
+	btcutil.Amount
+}
+
+// MarshalFlag satisifes the flags.Marshaler interface.
+func (a *AmountFlag) MarshalFlag() (string, error) {
+	return a.Amount.String(), nil
+}
+
+// UnmarshalFlag satisifes the flags.Unmarshaler interface.
+func (a *AmountFlag) UnmarshalFlag(value string) error {
+	value = strings.TrimSuffix(value, " BTC")
+	valueF64, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return err
+	}
+	amount, err := btcutil.NewAmount(valueF64)
+	if err != nil {
+		return err
+	}
+	a.Amount = amount
+	return nil
+}
