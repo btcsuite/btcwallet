7ea51177a91111e1d949f271546d51fc431894c4
cjepson <cjepson@decred.org>
2016-02-09 01:06:49 -0500

    Fix a rare panic importing discovered multisignature scripts
    A rare panic could occur importing multisignature scripts found in the
    blockchain that were unconfirmed. This avoids the nil dereference that
    causes the panic.

--- waddrmgr/manager.go
+++ waddrmgr/manager.go
@@ -1395,8 +1395,10 @@ func (m *Manager) ImportScript(script []byte,
 	// The start block needs to be updated when the newly imported address
 	// is before the current one.
 	updateStartBlock := false
-	if bs.Height < m.syncState.startBlock.Height {
-		updateStartBlock = true
+	if bs != nil {
+		if bs.Height < m.syncState.startBlock.Height {
+			updateStartBlock = true
+		}
 	}
 
 	// Save the new imported address to the db and update start block (if
