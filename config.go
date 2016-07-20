fe608bd83cb8d3b4ed446035f5127721f1706553
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2016-07-20 17:22:14 -0500

    Add options --nopass and --seed.
    Both of these options are used with --create, and they are intended
    to make it easier to manage wallet creation from the command line.
    
    --nopass means that the user is not prompted for a password to
    encrypt the wallet (a default password is used instead).
    
    --seed enables the user to provide the seed to the wallet from the
    command line.

--- config.go
+++ config.go
@@ -47,8 +47,6 @@ type config struct {
 	// General application behavior
 	ConfigFile    *cfgutil.ExplicitString `short:"C" long:"configfile" description:"Path to configuration file"`
 	ShowVersion   bool                    `short:"V" long:"version" description:"Display version information and exit"`
-	Create        bool                    `long:"create" description:"Create the wallet if it does not exist"`
-	CreateTemp    bool                    `long:"createtemp" description:"Create a temporary simulation wallet (pass=password) in the data directory indicated; must call with --datadir"`
 	AppDataDir    *cfgutil.ExplicitString `short:"A" long:"appdata" description:"Application data directory for wallet config, databases and logs"`
 	TestNet3      bool                    `long:"testnet" description:"Use the test Bitcoin network (version 3) (default mainnet)"`
 	SimNet        bool                    `long:"simnet" description:"Use the simulation test network (default mainnet)"`
@@ -58,7 +56,11 @@ type config struct {
 	Profile       string                  `long:"profile" description:"Enable HTTP profiling on given port -- NOTE port must be between 1024 and 65536"`
 
 	// Wallet options
-	WalletPass string `long:"walletpass" default-mask:"-" description:"The public wallet password -- Only required if the wallet was created with one"`
+	WalletPass string  `long:"walletpass" default-mask:"-" description:"The public wallet password -- Only required if the wallet was created with one"`
+	Create     bool    `long:"create" description:"Create the wallet if it does not exist"`
+	CreateTemp bool    `long:"createtemp" description:"Create a temporary simulation wallet (pass=password) in the data directory indicated; must call with --datadir"`
+	Seed       *string `long:"seed" description:"The seed for a new wallet."`
+	NoPass     bool    `long:"nopass" description:"If this option is set, the wallet is stored unencrypted. Only used with --create."`
 
 	// RPC client options
 	RPCConnect       string                  `short:"c" long:"rpcconnect" description:"Hostname/IP and port of btcd RPC server to connect to (default localhost:8334, testnet: localhost:18334, simnet: localhost:18556)"`
@@ -417,6 +419,20 @@ func loadConfig() (*config, []string, error) {
 
 	dbFileExists, err := cfgutil.FileExists(dbPath)
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil, nil, err
+	}
+
+	if cfg.NoPass && !cfg.Create {
+		// Cannot use nopass options except with create.
+		err = fmt.Errorf("Cannot use --nopass except with --create")
+		fmt.Fprintln(os.Stderr, err)
+		return nil, nil, err
+	}
+
+	if cfg.Seed != nil && !cfg.Create {
+		// Cannot use seed options except with create.
+		err = fmt.Errorf("Cannot use --seed except with --create")
 		fmt.Fprintln(os.Stderr, err)
 		return nil, nil, err
 	}
