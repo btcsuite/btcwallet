vertcoin-project/vtcwallet

wtxmgr: when creating a TxRecord don't compute txid manually
This fixes a bug introduced by the new segwit behavior upstream in
btcd. Previously it was a nice optimization to skip the extra
serialization, and compute the txid manually from the serialized
transaction.

However, with segwit, doing that will generate an invalid txid, since
txid’s should ignore any witness data. Therefore, we now fallback to
using msgTx.TxSha() which serializes the transaction without witness
data before calculating the txid.

wtxmgr/tx.go
@@ -109,8 +109,9 @@ func NewTxRecordFromMsgTx(msgTx *wire.MsgTx, received time.Time) (*TxRecord, err
 		MsgTx:        *msgTx,
 		Received:     received,
 		SerializedTx: buf.Bytes(),
+		Hash:         msgTx.TxSha(),
 	}
-	copy(rec.Hash[:], wire.DoubleSha256(rec.SerializedTx))
+
 	return rec, nil
 }
 

btcsuite/btcwallet

wtxmgr/tx.go
@@ -111,8 +111,8 @@ func NewTxRecordFromMsgTx(msgTx *wire.MsgTx, received time.Time) (*TxRecord, err
 		MsgTx:        *msgTx,
 		Received:     received,
 		SerializedTx: buf.Bytes(),
+		Hash:         msgTx.TxHash(),
 	}
-	copy(rec.Hash[:], chainhash.DoubleHashB(rec.SerializedTx))
 	return rec, nil
 }
 