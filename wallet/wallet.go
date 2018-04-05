bb38b9f655f85504d8ebafb30469801d5fde347b
Kenneth Perry (thothonegan) <thothonegan@gmail.com>
2018-04-05 17:58:14 -0500

    When determining whether to rollback, ask the chain using the chainhash instead of the possibly invalid local hash

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -489,7 +489,7 @@ func (w *Wallet) syncWithChain() error {
 			if err != nil {
 				return err
 			}
-			header, err := chainClient.GetBlockHeader(hash)
+			header, err := chainClient.GetBlockHeader(chainHash)
 			if err != nil {
 				return err
 			}
