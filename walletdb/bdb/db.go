9da1b21aef93a8856a808e7e341a7fdd2cccc9d7
David Hill <dhill@mindcry.org>
2020-05-13 09:57:22 -0400

    walletdb: update bbolt mode - fixes panic on go1.13+

--- walletdb/bdb/db.go
+++ walletdb/bdb/db.go
@@ -9,7 +9,7 @@ import (
 	"os"
 
 	"github.com/btcsuite/btcwallet/walletdb"
-	"github.com/coreos/bbolt"
+	"go.etcd.io/bbolt"
 )
 
 // convertErr converts some bolt errors to the equivalent walletdb error.
