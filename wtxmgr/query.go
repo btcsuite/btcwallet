82e0a9cdfee938edcf1d06c4d5db9ffbd8aa838c
Olaoluwa Osuntokun <laolu32@gmail.com>
2022-02-04 16:01:07 -0800

    wtxmgr: update module to latest btcd/btcutil deps

--- wtxmgr/query.go
+++ wtxmgr/query.go
@@ -8,8 +8,8 @@ package wtxmgr
 import (
 	"fmt"
 
+	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcwallet/walletdb"
 )
 
