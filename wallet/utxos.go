9a650e50e69470247e73a19cd6e59a1fc4014c24
yyforyongyu <yong2452@gmail.com>
2025-08-26 01:46:52 +0800

    wallet: rename `w.Manager` to `w.addrStore`
    To make it clear that it's accessing the addr store and we also make it
    private so it cannot be accessed outside the package.

--- wallet/utxos.go
+++ wallet/utxos.go
@@ -44,7 +44,7 @@ func (w *Wallet) UnspentOutputs(policy OutputSelectionPolicy) ([]*TransactionOut
 		addrmgrNs := tx.ReadBucket(waddrmgrNamespaceKey)
 		txmgrNs := tx.ReadBucket(wtxmgrNamespaceKey)
 
-		syncBlock := w.Manager.SyncedTo()
+		syncBlock := w.addrStore.SyncedTo()
 
 		// TODO: actually stream outputs from the db instead of fetching
 		// all of them at once.
@@ -70,7 +70,7 @@ func (w *Wallet) UnspentOutputs(policy OutputSelectionPolicy) ([]*TransactionOut
 				// per output.
 				continue
 			}
-			_, outputAcct, err := w.Manager.AddrAccount(addrmgrNs, addrs[0])
+			_, outputAcct, err := w.addrStore.AddrAccount(addrmgrNs, addrs[0])
 			if err != nil {
 				return err
 			}
