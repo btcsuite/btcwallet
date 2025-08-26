72f643cebe117bc5d2e9069e98cb3787015be9da
yyforyongyu <yong2452@gmail.com>
2025-08-26 20:34:29 +0800

    wallet: implement `NewAccount`
    This method behaves almost the same as `NextAccount`, we also create a
    new file to track all the deprecated methods. We still need to keep them
    for now to satisfy the `wallet.Interface` and stay backwards-compatible.

---
+++ wallet/account_manager.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2025 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package wallet
+
+import (
+	"context"
+
+	"github.com/btcsuite/btcwallet/waddrmgr"
+	"github.com/btcsuite/btcwallet/walletdb"
+)
+
+// NewAccount creates the next account and returns its account number. The name
+// must be unique under the kep scope. In order to support automatic seed
+// restoring, new accounts may not be created when all of the previous 100
+// accounts have no transaction history (this is a deviation from the BIP0044
+// spec, which allows no unused account gaps).
+func (w *Wallet) NewAccount(_ context.Context, scope waddrmgr.KeyScope,
+	name string) (uint32, error) {
+
+	manager, err := w.addrStore.FetchScopedKeyManager(scope)
+	if err != nil {
+		return 0, err
+	}
+
+	// Validate that the scope manager can add this new account.
+	err = manager.CanAddAccount()
+	if err != nil {
+		return 0, err
+	}
+
+	var (
+		accountIndex uint32
+	)
+	err = walletdb.Update(w.db, func(tx walletdb.ReadWriteTx) error {
+		addrmgrNs := tx.ReadWriteBucket(waddrmgrNamespaceKey)
+
+		// Create a new account under the current key scope.
+		accountID, err := manager.NewAccount(addrmgrNs, name)
+		accountIndex = accountID
+
+		return err
+	})
+
+	return accountIndex, err
+}
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2049,46 +2049,6 @@ func (w *Wallet) RenameAccount(scope waddrmgr.KeyScope, account uint32, newName 
 	return err
 }
 
-// NextAccount creates the next account and returns its account number.  The
-// name must be unique to the account.  In order to support automatic seed
-// restoring, new accounts may not be created when all of the previous 100
-// accounts have no transaction history (this is a deviation from the BIP0044
-// spec, which allows no unused account gaps).
-func (w *Wallet) NextAccount(scope waddrmgr.KeyScope, name string) (uint32, error) {
-	manager, err := w.addrStore.FetchScopedKeyManager(scope)
-	if err != nil {
-		return 0, err
-	}
-
-	// Validate that the scope manager can add this new account.
-	err = manager.CanAddAccount()
-	if err != nil {
-		return 0, err
-	}
-
-	var (
-		account uint32
-		props   *waddrmgr.AccountProperties
-	)
-	err = walletdb.Update(w.db, func(tx walletdb.ReadWriteTx) error {
-		addrmgrNs := tx.ReadWriteBucket(waddrmgrNamespaceKey)
-		var err error
-		account, err = manager.NewAccount(addrmgrNs, name)
-		if err != nil {
-			return err
-		}
-		props, err = manager.AccountProperties(addrmgrNs, account)
-		return err
-	})
-	if err != nil {
-		log.Errorf("Cannot fetch new account properties for notification "+
-			"after account creation: %v", err)
-	} else {
-		w.NtfnServer.notifyAccountProperties(props)
-	}
-	return account, err
-}
-
 // CreditCategory describes the type of wallet transaction output.  The category
 // of "sent transactions" (debits) is always "send", and is not expressed by
 // this type.
