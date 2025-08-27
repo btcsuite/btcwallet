23816418524e4f8bbd2e68623c35e244b54f144b
yyforyongyu <yong2452@gmail.com>
2025-08-27 21:54:00 +0800

    waddrmgr+wallet: move `ValidateAccountName` outside of the db tx

--- waddrmgr/scoped_manager.go
+++ waddrmgr/scoped_manager.go
@@ -1829,11 +1829,6 @@ func (s *ScopedKeyManager) RenameAccount(ns walletdb.ReadWriteBucket,
 		return managerError(ErrDuplicateAccount, str, err)
 	}
 
-	// Validate account name
-	if err := ValidateAccountName(name); err != nil {
-		return err
-	}
-
 	rowInterface, err := fetchAccountInfo(ns, &s.scope, account)
 	if err != nil {
 		return err
