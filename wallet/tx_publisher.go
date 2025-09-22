ae10daa7d92c94d05bf3303b3ecd03b4a9637856
yyforyongyu <yong2452@gmail.com>
2025-09-23 01:52:03 +0800

    wallet: implement `CheckMempoolAcceptance`

--- wallet/tx_publisher.go
+++ wallet/tx_publisher.go
@@ -34,3 +34,48 @@ type TxPublisher interface {
 	// Broadcast broadcasts a transaction to the network.
 	Broadcast(ctx context.Context, tx *wire.MsgTx, label string) error
 }
+
+// CheckMempoolAcceptance checks if a transaction would be accepted by the
+// mempool without broadcasting.
+func (w *Wallet) CheckMempoolAcceptance(_ context.Context,
+	tx *wire.MsgTx) error {
+
+	// TODO(yy): thread context through.
+	chainClient, err := w.requireChainClient()
+	if err != nil {
+		return err
+	}
+
+	// The TestMempoolAccept rpc expects a slice of transactions.
+	txns := []*wire.MsgTx{tx}
+
+	// Use a max feerate of 0 means the default value will be used when
+	// testing mempool acceptance. The default max feerate is 0.10 BTC/kvb,
+	// or 10,000 sat/vb.
+	maxFeeRate := float64(0)
+
+	results, err := chainClient.TestMempoolAccept(txns, maxFeeRate)
+	if err != nil {
+		return err
+	}
+
+	// Sanity check that the expected single result is returned.
+	if len(results) != 1 {
+		return errMempoolAccept
+	}
+
+	result := results[0]
+
+	// If the transaction is allowed, we can return early.
+	if result.Allowed {
+		return nil
+	}
+
+	// Otherwise, we'll map the reason to a concrete error type and return
+	// it.
+	//
+	//nolint:err113
+	err = errors.New(result.RejectReason)
+
+	return chainClient.MapRPCErr(err)
+}
