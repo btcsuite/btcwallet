805cbc5a75bfe17d025a9754bc716488b1a34af9
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-09-22 03:37:19 +0000

    wallet: benchmark `ListAccounts` API

--- wallet/account_manager_benchmark_test.go
+++ wallet/account_manager_benchmark_test.go
@@ -60,3 +60,55 @@ func BenchmarkListAccountsByScopeAPI(b *testing.B) {
 		})
 	}
 }
+
+// BenchmarkListAccountsAPI benchmarks ListAccounts API and a deprecated variant
+// of it using same key scopes and identical test data across multiple dataset
+// sizes. Test names start with dataset size to group API comparisons for
+// benchstat analysis.
+func BenchmarkListAccountsAPI(b *testing.B) {
+	benchmarkSizes := generateBenchmarkSizes(
+		benchmarkConfig{
+			accountGrowth: linearGrowth,
+			utxoGrowth:    exponentialGrowth,
+			maxIterations: 14,
+			startIndex:    0,
+		},
+	)
+	scopes := waddrmgr.DefaultKeyScopes
+
+	for _, size := range benchmarkSizes {
+		b.Run(size.name()+"/0-Before", func(b *testing.B) {
+			w := setupBenchmarkWallet(
+				b, benchmarkWalletConfig{
+					scopes:      scopes,
+					numAccounts: size.numAccounts,
+					numUTXOs:    size.numUTXOs,
+				},
+			)
+
+			b.ReportAllocs()
+			b.ResetTimer()
+			for b.Loop() {
+				_, err := listAccountsDeprecated(w)
+				require.NoError(b, err)
+			}
+		})
+
+		b.Run(size.name()+"/1-After", func(b *testing.B) {
+			w := setupBenchmarkWallet(
+				b, benchmarkWalletConfig{
+					scopes:      scopes,
+					numAccounts: size.numAccounts,
+					numUTXOs:    size.numUTXOs,
+				},
+			)
+
+			b.ReportAllocs()
+			b.ResetTimer()
+			for b.Loop() {
+				_, err := w.ListAccounts(b.Context())
+				require.NoError(b, err)
+			}
+		})
+	}
+}
