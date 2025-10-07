bd5094eda49f8558260992dd7dbdad8ac668e68d
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-10-07 15:40:03 +0000

    wallet: benchmark `LeaseOutput` API

--- wallet/utxo_manager_benchmark_test.go
+++ wallet/utxo_manager_benchmark_test.go
@@ -3,8 +3,10 @@ package wallet
 import (
 	"math"
 	"testing"
+	"time"
 
 	"github.com/btcsuite/btcwallet/waddrmgr"
+	"github.com/btcsuite/btcwallet/wtxmgr"
 	"github.com/stretchr/testify/require"
 )
 
@@ -136,6 +138,74 @@ func BenchmarkListUnspentAPI(b *testing.B) {
 						MinConfs: int32(minConfs),
 						MaxConfs: int32(maxConfs),
 					},
+				)
+				require.NoError(b, err)
+			}
+		})
+	}
+}
+
+// BenchmarkLeaseOutputAPI benchmarks LeaseOutput API and its deprecated
+// variant LeaseOutputDeprecated. Although LeaseOutput is an O(1) operation,
+// testing across different dataset sizes helps identify any database bucket
+// depth effects or positional bias as the UTXO set grows.
+func BenchmarkLeaseOutputAPI(b *testing.B) {
+	benchmarkSizes, namingInfo := generateBenchmarkSizes(
+		benchmarkConfig{
+			accountGrowth: constantGrowth,
+			utxoGrowth:    linearGrowth,
+			addressGrowth: constantGrowth,
+			maxIterations: 14,
+			startIndex:    0,
+		},
+	)
+	scopes := []waddrmgr.KeyScope{waddrmgr.KeyScopeBIP0084}
+	lockID := wtxmgr.LockID{0x01, 0x02, 0x03, 0x04}
+	duration := time.Hour
+
+	for _, size := range benchmarkSizes {
+		b.Run(size.name(namingInfo)+"/0-Before", func(b *testing.B) {
+			bw := setupBenchmarkWallet(
+				b, benchmarkWalletConfig{
+					scopes:       scopes,
+					numAccounts:  size.numAccounts,
+					numAddresses: size.numAddresses,
+					numUTXOs:     size.numUTXOs,
+				},
+			)
+
+			testOutpoint := getTestUtxoOutpoint(bw.outpoints)
+
+			b.ReportAllocs()
+			b.ResetTimer()
+
+			for b.Loop() {
+				_, err := bw.LeaseOutputDeprecated(
+					lockID, testOutpoint, duration,
+				)
+				require.NoError(b, err)
+			}
+		})
+
+		b.Run(size.name(namingInfo)+"/1-After", func(b *testing.B) {
+			bw := setupBenchmarkWallet(
+				b, benchmarkWalletConfig{
+					scopes:       scopes,
+					numAccounts:  size.numAccounts,
+					numAddresses: size.numAddresses,
+					numUTXOs:     size.numUTXOs,
+				},
+			)
+
+			testOutpoint := getTestUtxoOutpoint(bw.outpoints)
+
+			b.ReportAllocs()
+			b.ResetTimer()
+
+			for b.Loop() {
+				_, err := bw.LeaseOutput(
+					b.Context(), lockID, testOutpoint,
+					duration,
 				)
 				require.NoError(b, err)
 			}
