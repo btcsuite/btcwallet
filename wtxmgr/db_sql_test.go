5922103484c9d176c5d20d0180400d7b91c776a2
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-10-09 14:00:49 +0000

    wtxgmr: can create `block` SQL-based table

---
+++ wtxmgr/db_sql_test.go
@@ -0,0 +1,122 @@
+// Copyright (c) 2025 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package wtxmgr
+
+import (
+	"database/sql"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/require"
+)
+
+// loadSchema reads the actual production migration schema.
+// This ensures tests use the exact same schema as production.
+func loadSchema(t *testing.T, schema string) string {
+	t.Helper()
+
+	cwd, err := os.Getwd()
+	require.NoError(t, err)
+
+	parentDir := filepath.Dir(cwd)
+
+	schemaPath := filepath.Join(
+		parentDir, "internal", "store", "sqldb", "migrations", schema,
+	)
+	schemaBytes, err := os.ReadFile(schemaPath)
+	require.NoError(t, err)
+
+	return string(schemaBytes)
+}
+
+// TestPutBlockRecordSQL tests inserting block records into SQL database.
+func TestPutBlockRecordSQL(t *testing.T) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	t.Cleanup(func() {
+		db.Close()
+	})
+	require.NoError(t, err)
+
+	schemaFile := "000002__blocks.up.sql"
+	schemaLoaded := loadSchema(t, schemaFile)
+
+	_, err = db.Exec(schemaLoaded)
+	require.NoError(t, err)
+
+	store := NewSQLStore(db)
+
+	// Test Case 1: Insert a new block with first transaction.
+	hash1, _ := chainhash.NewHashFromStr(
+		"00000000000000017188b968a371bab95aa43522665353b646e41865abae" +
+			"02a4",
+	)
+	txHash1, _ := chainhash.NewHashFromStr(
+		"8c18cf9ad4a950f7bd0174da3ddbde77269091c47ac3a7471fbd9888953f" +
+			"4645",
+	)
+
+	block1 := &BlockMeta{
+		Block: Block{
+			Height: 100,
+			Hash:   *hash1,
+		},
+		Time: time.Unix(1387737310, 0),
+	}
+
+	err = store.putBlockRecordSQL(t.Context(), block1, txHash1)
+	require.NoError(t, err)
+
+	// Verify block was inserted correctly.
+	block, err := store.queries.GetBlockByHeight(t.Context(), 100)
+	require.NoError(t, err, "Failed to query block")
+	require.Equal(t, int64(100), block.BlockHeight)
+	require.Equal(t, int64(1387737310), block.Timestamp)
+	require.Equal(t, 32, len(block.HeaderHash))
+
+	// Test Case 2: Add another transaction to the same block.
+	txHash2, _ := chainhash.NewHashFromStr(
+		"9c18cf9ad4a950f7bd0174da3ddbde77269091c47ac3a7471fbd9888953f" +
+			"4646",
+	)
+
+	err = store.putBlockRecordSQL(t.Context(), block1, txHash2)
+	require.NoError(t, err, "Failed to add transaction to existing block")
+
+	// Verify block still exists (should be idempotent).
+	block2, err := store.queries.GetBlockByHeight(t.Context(), 100)
+	require.NoError(t, err, "Failed to query block after second insert")
+	require.Equal(t, int64(100), block2.BlockHeight)
+
+	// Test Case 3: Insert a different block.
+	hash2, _ := chainhash.NewHashFromStr(
+		"00000000000000027188b968a371bab95aa43522665353b646e41865abae" +
+			"02a4",
+	)
+	txHash3, _ := chainhash.NewHashFromStr(
+		"7c18cf9ad4a950f7bd0174da3ddbde77269091c47ac3a7471fbd9888953f" +
+			"4647",
+	)
+
+	block3 := &BlockMeta{
+		Block: Block{
+			Height: 101,
+			Hash:   *hash2,
+		},
+		Time: time.Unix(1387737320, 0),
+	}
+
+	err = store.putBlockRecordSQL(t.Context(), block3, txHash3)
+	require.NoError(t, err)
+
+	_, err = store.queries.GetBlockByHeight(t.Context(), 100)
+	require.NoError(t, err)
+
+	_, err = store.queries.GetBlockByHeight(t.Context(), 101)
+	require.NoError(t, err)
+}
--- wtxmgr/go.mod
+++ wtxmgr/go.mod
@@ -1,25 +1,29 @@
 module github.com/btcsuite/btcwallet/wtxmgr
 
 require (
-	github.com/btcsuite/btcd v0.23.4
-	github.com/btcsuite/btcd/btcutil v1.1.0
-	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1
-	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f
+	github.com/btcsuite/btcd v0.24.3-0.20250318170759-4f4ea81776d6
+	github.com/btcsuite/btcd/btcutil v1.1.6
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0
+	github.com/btcsuite/btclog v1.0.0
+	github.com/btcsuite/btcwallet v0.0.0
 	github.com/btcsuite/btcwallet/walletdb v1.5.1
 	github.com/lightningnetwork/lnd/clock v1.0.1
-	github.com/stretchr/testify v1.9.0
+	github.com/mattn/go-sqlite3 v1.14.32
+	github.com/stretchr/testify v1.11.1
 )
 
+replace github.com/btcsuite/btcwallet => ../
+
 require (
-	github.com/btcsuite/btcd/btcec/v2 v2.1.3 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.1.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.etcd.io/bbolt v1.3.11 // indirect
-	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
-go 1.22
+go 1.24.6
