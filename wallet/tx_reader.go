33087f64133b8653413c415f716c840a69481d9e
yyforyongyu <yong2452@gmail.com>
2025-10-15 13:16:44 +0800

    unit: use big.Rat for fee rates
    This commit refactors the fee rate types to use big.Rat for precise
    calculations that support sub-satoshi values. This avoids floating-point
    inaccuracies while allowing for fractional fee rates.
    
    The fee rate structs now embed a *big.Rat, and all associated methods
    have been updated to use rational arithmetic. Comparison methods
    (Equal, GreaterThan, etc.) have been added to provide a convenient and
    type-safe way to compare different fee rates.

--- wallet/tx_reader.go
+++ wallet/tx_reader.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"math/big"
 	"time"
 
 	"github.com/btcsuite/btcd/address/v2"
@@ -283,6 +284,7 @@ func (w *Wallet) buildBasicTxDetail(txDetails *wtxmgr.TxDetails) *TxDetail {
 		Label:        txDetails.Label,
 		ReceivedTime: txDetails.Received,
 		Weight:       safeInt64ToWeightUnit(txWeight),
+		FeeRate:      unit.SatPerVByte{Rat: big.NewRat(0, 1)},
 	}
 }
 
