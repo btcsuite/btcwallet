7416283c8655c232c5d0618cbed9468b97a54948
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-11-05 18:36:21 +0000

    internal: remove non-functional sql files

--- internal/db/sqldb/migrations/000001__dummy.up.sql
+++
@@ -1,5 +0,0 @@
--- Dummy migration to provide initial SQLC material.
--- Should be removed once we have real migrations.
-CREATE TABLE "dummy" (
-    "id" INTEGER PRIMARY KEY
-);
--- internal/db/sqldb/sqlc/queries/dummy.sql
+++
@@ -1,8 +0,0 @@
--- name: GetDummyById :one
-SELECT id FROM dummy WHERE id = $1;
-
--- name: InsertDummy :exec
-INSERT INTO dummy (id) VALUES ($1);
-
--- name: DeleteDummy :exec
-DELETE FROM dummy WHERE id = $1;
