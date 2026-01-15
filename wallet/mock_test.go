c699d48e371a45e87e111d507c8a296132afe015
yyforyongyu <yong2452@gmail.com>
2026-01-16 02:31:29 +0800

    wallet: add mockNeutrinoChain for interface compliance
    Introduce mockNeutrinoChain struct in mock_test.go, which embeds
    mockChain and explicitly implements the chain.NeutrinoChainService
    interface. This resolves method signature conflicts between
    chain.Interface and NeutrinoChainService (specifically for GetBlock and
    GetCFilter) and allows for robust testing of Neutrino-specific logic
    without polluting the general mockChain.

--- wallet/mock_test.go
+++ wallet/mock_test.go
@@ -25,6 +25,9 @@ import (
 	"github.com/btcsuite/btcwallet/waddrmgr"
 	"github.com/btcsuite/btcwallet/walletdb"
 	"github.com/btcsuite/btcwallet/wtxmgr"
+	"github.com/lightninglabs/neutrino"
+	"github.com/lightninglabs/neutrino/banman"
+	"github.com/lightninglabs/neutrino/headerfs"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -1033,6 +1036,173 @@ func (m *mockChain) SubmitPackage(txns []*wire.MsgTx,
 func (m *mockChain) MapRPCErr(err error) error {
 	args := m.Called(err)
 	return args.Error(0)
+}
+
+// mockNeutrinoChain is a mock implementation of the chain.NeutrinoChainService
+// interface.
+type mockNeutrinoChain struct {
+	mockChain
+}
+
+// A compile-time assertion to ensure that mockNeutrinoChain implements the
+// chain.NeutrinoChainService.
+var _ chain.NeutrinoChainService = (*mockNeutrinoChain)(nil)
+
+// Stop implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) Stop() error {
+	args := m.Called()
+	return args.Error(0)
+}
+
+// GetBlock implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) GetBlock(hash chainhash.Hash,
+	opts ...neutrino.QueryOption) (*btcutil.Block, error) {
+
+	args := m.Called(hash, opts)
+	if args.Get(0) != nil {
+		if val, ok := args.Get(0).(*btcutil.Block); ok {
+			return val, args.Error(1)
+		}
+	}
+
+	return nil, args.Error(1)
+}
+
+// GetCFilter implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) GetCFilter(hash chainhash.Hash,
+	filterType wire.FilterType,
+	opts ...neutrino.QueryOption) (*gcs.Filter, error) {
+
+	args := m.Called(hash, filterType, opts)
+	if args.Get(0) != nil {
+		if val, ok := args.Get(0).(*gcs.Filter); ok {
+			return val, args.Error(1)
+		}
+	}
+
+	return nil, args.Error(1)
+}
+
+// GetBlockHeight implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) GetBlockHeight(
+	hash *chainhash.Hash) (int32, error) {
+
+	args := m.Called(hash)
+	return args.Get(0).(int32), args.Error(1)
+}
+
+// BestBlock implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) BestBlock() (*headerfs.BlockStamp, error) {
+	args := m.Called()
+	if args.Get(0) != nil {
+		if val, ok := args.Get(0).(*headerfs.BlockStamp); ok {
+			return val, args.Error(1)
+		}
+	}
+
+	return nil, args.Error(1)
+}
+
+// SendTransaction implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) SendTransaction(tx *wire.MsgTx) error {
+	args := m.Called(tx)
+	return args.Error(0)
+}
+
+// GetUtxo implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) GetUtxo(
+	opts ...neutrino.RescanOption) (*neutrino.SpendReport, error) {
+
+	args := m.Called(opts)
+	if args.Get(0) != nil {
+		if val, ok := args.Get(0).(*neutrino.SpendReport); ok {
+			return val, args.Error(1)
+		}
+	}
+
+	return nil, args.Error(1)
+}
+
+// BanPeer implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) BanPeer(addr string,
+	reason banman.Reason) error {
+
+	args := m.Called(addr, reason)
+	return args.Error(0)
+}
+
+// IsBanned implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) IsBanned(addr string) bool {
+	args := m.Called(addr)
+	return args.Bool(0)
+}
+
+// AddPeer implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) AddPeer(peer *neutrino.ServerPeer) {
+	m.Called(peer)
+}
+
+// AddBytesSent implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) AddBytesSent(bytes uint64) {
+	m.Called(bytes)
+}
+
+// AddBytesReceived implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) AddBytesReceived(bytes uint64) {
+	m.Called(bytes)
+}
+
+// NetTotals implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) NetTotals() (uint64, uint64) {
+	args := m.Called()
+
+	var a, b uint64
+	if args.Get(0) != nil {
+		if val, ok := args.Get(0).(uint64); ok {
+			a = val
+		}
+	}
+
+	if args.Get(1) != nil {
+		if val, ok := args.Get(1).(uint64); ok {
+			b = val
+		}
+	}
+
+	return a, b
+}
+
+// UpdatePeerHeights implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) UpdatePeerHeights(hash *chainhash.Hash,
+	height int32, peer *neutrino.ServerPeer) {
+
+	m.Called(hash, height, peer)
+}
+
+// ChainParams implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) ChainParams() chaincfg.Params {
+	args := m.Called()
+	if args.Get(0) != nil {
+		if val, ok := args.Get(0).(chaincfg.Params); ok {
+			return val
+		}
+	}
+
+	return chaincfg.Params{}
+}
+
+// PeerByAddr implements the chain.NeutrinoChainService interface.
+func (m *mockNeutrinoChain) PeerByAddr(
+	addr string) *neutrino.ServerPeer {
+
+	args := m.Called(addr)
+	if args.Get(0) != nil {
+		if val, ok := args.Get(0).(*neutrino.ServerPeer); ok {
+			return val
+		}
+	}
+
+	return nil
 }
 
 // mockManagedPubKeyAddr is a mock implementation of the
