1b7aae85aa3d1cbc55cbcf695073e93bc807bc7c
Gustavo Stingelin <gustavo.stingelin@outlook.com>
2026-04-12 14:00:40 -0300

    wallet: fix addresses rollback objects

--- wallet/internal/sql/pg/migrations/000006_addresses.down.sql
+++ wallet/internal/sql/pg/migrations/000006_addresses.down.sql
@@ -1,5 +1,9 @@
 -- Rollback note: Idempotent by design (using "IF EXISTS").
 -- Must succeed even if tables are already dropped or database is in unexpected state.
+DROP TRIGGER IF EXISTS trg_addresses_imported_key_count_insert ON addresses;
+DROP TRIGGER IF EXISTS trg_addresses_imported_key_count_delete ON addresses;
+DROP FUNCTION IF EXISTS sync_account_imported_key_count_insert();
+DROP FUNCTION IF EXISTS sync_account_imported_key_count_delete();
 DROP INDEX IF EXISTS idx_addresses_account_id;
 DROP TABLE IF EXISTS address_secrets;
 DROP TABLE IF EXISTS addresses;
