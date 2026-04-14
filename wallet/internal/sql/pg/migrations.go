58a0e47a52d22ccfb45dbb8fd59d10734c5d77de
Gustavo Stingelin <gustavo.stingelin@outlook.com>
2026-04-14 00:56:57 -0300

    wallet: simplify migration instance creation

--- wallet/internal/sql/pg/migrations.go
+++ wallet/internal/sql/pg/migrations.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 
 	gomigrate "github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database"
 	migrate "github.com/golang-migrate/migrate/v4/database/postgres"
 	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
@@ -15,25 +14,34 @@ import (
 //go:embed migrations/*.sql
 var migrationFS embed.FS
 
-type driverFactory func(*sql.DB) (database.Driver, error)
-
-// applyMigrations applies all embedded postgres migrations to one database.
-func applyMigrations(db *sql.DB, newDriver driverFactory) error {
+// newMigrationInstance creates a migrate instance from embedded postgres
+// migrations.
+func newMigrationInstance(db *sql.DB) (*gomigrate.Migrate, error) {
 	sourceDriver, err := iofs.New(migrationFS, "migrations")
 	if err != nil {
-		return fmt.Errorf("create source driver: %w", err)
+		return nil, fmt.Errorf("create source driver: %w", err)
 	}
 
-	driver, err := newDriver(db)
+	driver, err := migrate.WithInstance(db, &migrate.Config{})
 	if err != nil {
-		return fmt.Errorf("create postgres driver: %w", err)
+		return nil, fmt.Errorf("create postgres driver: %w", err)
 	}
 
 	m, err := gomigrate.NewWithInstance(
 		"iofs", sourceDriver, "postgres", driver,
 	)
 	if err != nil {
-		return fmt.Errorf("create migrate instance: %w", err)
+		return nil, fmt.Errorf("create migrate instance: %w", err)
+	}
+
+	return m, nil
+}
+
+// ApplyMigrations applies all PostgreSQL migrations to the database.
+func ApplyMigrations(db *sql.DB) error {
+	m, err := newMigrationInstance(db)
+	if err != nil {
+		return err
 	}
 
 	err = m.Up()
@@ -42,11 +50,4 @@ func applyMigrations(db *sql.DB, newDriver driverFactory) error {
 	}
 
 	return nil
-}
-
-// ApplyMigrations applies all PostgreSQL migrations to the database.
-func ApplyMigrations(db *sql.DB) error {
-	return applyMigrations(db, func(db *sql.DB) (database.Driver, error) {
-		return migrate.WithInstance(db, &migrate.Config{})
-	})
 }
