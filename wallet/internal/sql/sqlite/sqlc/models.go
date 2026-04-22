58fba9ee50bf1ec40f66ac5e88dc2edba19a5aaa
yyforyongyu <yong2452@gmail.com>
2026-04-22 22:09:29 +0800

    sql/sqlite: add tx detail read queries
    Add the sqlite owned-edge detail queries and regenerate the sqlc
    bindings needed by the store-backed tx detail read path.

--- wallet/internal/sql/sqlite/sqlc/models.go
+++ wallet/internal/sql/sqlite/sqlc/models.go
@@ -80,15 +80,16 @@ type KeyScopeSecret struct {
 }
 
 type Transaction struct {
-	WalletID     int64
-	ID           int64
-	TxHash       []byte
-	RawTx        []byte
-	BlockHeight  sql.NullInt64
-	TxStatus     int64
-	ReceivedTime time.Time
-	IsCoinbase   bool
-	TxLabel      string
+	WalletID       int64
+	ID             int64
+	TxHash         []byte
+	RawTx          []byte
+	BlockHeight    sql.NullInt64
+	ConfirmedOrder sql.NullInt64
+	TxStatus       int64
+	ReceivedTime   time.Time
+	IsCoinbase     bool
+	TxLabel        string
 }
 
 type TxReplacement struct {
