45a8977f3f08987d0cdb6c6f23f5e9acb9a6ef8f
yyforyongyu <yong2452@gmail.com>
2026-05-13 00:30:17 +0800

    db: add AccountStore to Store

---
+++ wallet/internal/db/kvdb/accountstore.go
@@ -0,0 +1,42 @@
+package kvdb
+
+import (
+	"context"
+
+	"github.com/btcsuite/btcwallet/wallet/internal/db"
+)
+
+// CreateDerivedAccount is not yet implemented for kvdb.
+func (s *Store) CreateDerivedAccount(ctx context.Context,
+	_ db.CreateDerivedAccountParams) (*db.AccountInfo, error) {
+
+	return nil, notImplemented(ctx, "CreateDerivedAccount")
+}
+
+// CreateImportedAccount is not yet implemented for kvdb.
+func (s *Store) CreateImportedAccount(ctx context.Context,
+	_ db.CreateImportedAccountParams) (*db.AccountProperties, error) {
+
+	return nil, notImplemented(ctx, "CreateImportedAccount")
+}
+
+// GetAccount is not yet implemented for kvdb.
+func (s *Store) GetAccount(ctx context.Context,
+	_ db.GetAccountQuery) (*db.AccountInfo, error) {
+
+	return nil, notImplemented(ctx, "GetAccount")
+}
+
+// ListAccounts is not yet implemented for kvdb.
+func (s *Store) ListAccounts(ctx context.Context,
+	_ db.ListAccountsQuery) ([]db.AccountInfo, error) {
+
+	return nil, notImplemented(ctx, "ListAccounts")
+}
+
+// RenameAccount is not yet implemented for kvdb.
+func (s *Store) RenameAccount(ctx context.Context,
+	_ db.RenameAccountParams) error {
+
+	return notImplemented(ctx, "RenameAccount")
+}
