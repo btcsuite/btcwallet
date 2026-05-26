107d5f79ef25fef7df407529beb2bfde540ebd60
yyforyongyu <yong2452@gmail.com>
2026-05-26 19:29:38 +0800

    bwtest/mock: add package skeleton
    Add package documentation for the shared test-double package. The
    package name carries the mock semantics while exported type names match
    the interfaces they implement.

---
+++ bwtest/mock/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2026 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// Package mock contains shared testify-based test doubles.
+package mock
